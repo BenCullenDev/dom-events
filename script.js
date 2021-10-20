bf6b455158208c52da62eaf5c44ce3004366cb0c
Ben Cullen <benjaminjcullen1@gmail.com>
2021-10-20 13:25:11 +0100

    added create new li on click

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-var button = document.querySelector("button");
+var button = document.getElementById("enter");
+var input = document.getElementById("input");
+var ul = document.querySelector("ul");
 
 button.addEventListener("click", function() {
-    console.log("You clicked the button");
+    if (input.value.length > 0) {
+        console.log("You clicked the button");
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+    }
+    
 })
