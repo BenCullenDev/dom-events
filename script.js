55bf8a0881820bd22485cf530ad5956713ec4fda
Ben Cullen <benjaminjcullen1@gmail.com>
2021-10-21 09:29:22 +0100

    refactored by extracting funtions

--- script.js
+++ script.js
@@ -2,24 +2,30 @@ var button = document.getElementById("enter");
 var input = document.getElementById("input");
 var ul = document.querySelector("ul");
 
-button.addEventListener("click", function() {
-    if (input.value.length > 0) {
-        console.log("You clicked the button");
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value = "";
-    }
-    
-})
+function inputLength() {
+    return input.value.length;
+}
 
-input.addEventListener("keypress", function() {
-    if (input.value.length > 0 && event.keyCode === 13) {
-        console.log("You clicked the button");
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value = "";
+function createListElement() {
+    console.log("You clicked the button");
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+}
+
+function addListAfterClick() {
+    if (inputLength() > 0) {
+        createListElement();
     }
-    
-})
+}
+
+function addListAfterEnter() {
+    if (inputLength() > 0 && event.keyCode === 13) {
+        createListElement();
+    }
+    }
+
+button.addEventListener("click", addListAfterClick)
+
+input.addEventListener("keypress", addListAfterEnter)
